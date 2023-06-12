92fcdb4c85dd8a3b2d1fdd0721fe0668f8e9fa86
Exequiel65 <marcos97britos@gmail.com>
2023-06-12 20:13:02 -0300

    finished class Bases de Angular

--- 01-base/src/app/app.module.ts
+++ 01-base/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/components/counter.module';
 
+import { AppComponent } from './app.component';
+import { HerosModule } from './heroes/heros.module';
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HerosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
